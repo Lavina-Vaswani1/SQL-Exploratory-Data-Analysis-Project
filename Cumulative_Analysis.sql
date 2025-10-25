44474d1a4f53ef8890e3e43cd608529128c00f8c
Vaswani_Lavina <lavina.vaswani02@gmail.com>
2025-10-25 21:39:49 +0530

    Refine comments in Cumulative_Analysis.sql
    Updated comments for clarity and consistency in the SQL script.

--- Cumulative_Analysis.sql
+++ Cumulative_Analysis.sql
@@ -1,5 +1,5 @@
-------------------------------------------------------------- CUMULATIVE ANALYSIS-----------------------------------------------------------------------------------
-------------------------------------- CALCULATE THE TOTAL SALES FOR EACH MONTH AND RUNNING TOTAL OF SALES OVER TIME-----------------------------------------------
+------------------------------------------------------------- CUMULATIVE ANALYSIS-------------------------------------------------------------------------------------
+------------------------------------- CALCULATE THE TOTAL SALES FOR EACH MONTH AND RUNNING TOTAL OF SALES OVER TIME--------------------------------------------------
 
 select order_date,total_sales,sum(total_sales) over(order by order_date) from 
 (select date_format(order_date,"%Y")  as order_date,
@@ -7,7 +7,7 @@ sum(sales_amount) as total_sales from
 `gold.fact_sales`where date_format(order_date,"%Y") is not null group by
 date_format(order_date,"%Y")) as t;
 
---------------------------------------------------------------- CALCULATE THE TOTAL SALES FOR EACH MONTH AND Average_price OF SALES OVER TIME--------------------------------------------------------------------------
+--------------------------------------- CALCULATE THE TOTAL SALES FOR EACH MONTH AND Average_price OF SALES OVER TIME------------------------------------------------
 select order_date,total_sales,sum(total_sales) over(order by order_date),avg(average_price) over(order by order_date)
 as average_price from 
 (select date_format(order_date,"%Y")  as order_date,
